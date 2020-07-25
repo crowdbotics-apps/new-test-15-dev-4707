f6d331bd1e3b7b370b0ad26c6279b6c2c5f029fe
Crowdbotics <team@crowdbotics.com>
2020-07-25 12:51:00 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,18 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +26,21 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Test(models.Model):
+    "Generated Model"
+    r1 = models.ForeignKey(
+        "home.CustomText", on_delete=models.CASCADE, related_name="test_r1",
+    )
+    r2 = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="test_r2",
+    )
+    r3 = models.ForeignKey(
+        "home.HomePage", on_delete=models.CASCADE, related_name="test_r3",
+    )
